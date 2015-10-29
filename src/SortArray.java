9cb72e490e93c361be1fc2e5d71b25825dcacc7b
Angelica Barajas <barajasan@gmail.com>
2015-10-28 17:52:25 -0700

    SortArray

---
+++ src/SortArray.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+
+	public class SortArray {
+
+		public static void main(String[] args) {
+			
+			int[] aryNums;
+			aryNums = new int[6];
+			
+			aryNums[0] = 123;
+			aryNums[1] = 456;
+			aryNums[2] = 479;
+			aryNums[3] = 135;
+			aryNums[4] = 246;
+			aryNums[5] = 135;
+			
+			Arrays.sort(aryNums);
+			
+			int i;
+			
+			for (i=0; i < aryNums.length; i++) {
+				System.out.println("num:" + aryNums[i]);
+				
+			}
+			
+		}
+
+	}
+
